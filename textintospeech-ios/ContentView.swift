8f09c9ac349921cb70b193eabfb0f3ea804c2130
Rongo <rongo123321@gmail.com>
2026-07-02 10:23:53 +0300

    Initial Commit

---
+++ textintospeech-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  textintospeech-ios
+//
+//  Created by rongo on 02/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ textintospeech-ios/textintospeech_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  textintospeech_iosApp.swift
+//  textintospeech-ios
+//
+//  Created by rongo on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct textintospeech_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
